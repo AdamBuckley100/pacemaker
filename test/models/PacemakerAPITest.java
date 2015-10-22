904136ce0af9b0f1827a93f4484835aa028ae3ff
AdamBuckly <adambuckley@live.com>
2015-10-22 20:42:59 +0100

    PacemakerAPITest has been simplified since we share a fixture.

--- test/models/PacemakerAPITest.java
+++ test/models/PacemakerAPITest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.base.Optional;
+
 import controllers.PacemakerAPI;
 import static models.Fixtures.users;
 import static models.Fixtures.activities;
