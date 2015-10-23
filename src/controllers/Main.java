cd58190d9c9beface0eab28fa191ab44de39dc15
AdamBuckly <adambuckley@live.com>
2015-10-23 09:00:54 +0100

    three Maps serialized were completely independent now they are not independent.

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,22 +1,30 @@
 package controllers;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.Collection;
-//java.util.List is unused
-import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import models.User;
+import utils.Serializer;
+import utils.XMLSerializer;
 
 public class Main
 {
-  public static void main(String[] args) throws IOException
+  public static void main(String[] args) throws Exception
   {    
-	  PacemakerAPI pacemakerAPI = new PacemakerAPI();
+	    File  datastore = new File("datastore3.xml");
+	    Serializer serializer = new XMLSerializer(datastore);
+
+	    PacemakerAPI pacemakerAPI = new PacemakerAPI(serializer);
+	    if (datastore.isFile())
+	    {
+	      pacemakerAPI.load();
+	    }
 
 	    pacemakerAPI.createUser("Bart", "Simpson",   "bart@simpson.com", "secret");
 	    pacemakerAPI.createUser("Homer", "Simpson",  "homer@simpson.com", "secret");
@@ -26,17 +34,8 @@ public class Main
 	    System.out.println(users);
 
 	    User homer = pacemakerAPI.getUserByEmail("homer@simpson.com");
-	    System.out.println(homer);
-	    
-	    
+	    pacemakerAPI.createActivity(homer.id, "walk", "tramore", 1000);
 
-	    pacemakerAPI.deleteUser(homer.id);
-	    users = pacemakerAPI.getUsers();
-	    System.out.println(users);
-	    
-	    XStream xstream = new XStream(new DomDriver());
-	    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("datastore.xml"));
-	    out.writeObject(users);
-	    out.close();
+	    pacemakerAPI.store(); 
   }
 }
--- src/controllers/PacemakerAPI.java
+++ src/controllers/PacemakerAPI.java
@@ -1,9 +1,20 @@
 package controllers;
+import utils.Serializer;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
+
 import com.google.common.base.Optional;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
 import java.util.HashMap;
 import java.util.Map;
+
 import models.Activity;
 import models.Location;
 import models.User;
@@ -13,22 +24,44 @@ public class PacemakerAPI
   private Map<Long,   User>   userIndex       = new HashMap<>();
   private Map<String, User>   emailIndex      = new HashMap<>();
   private Map<Long, Activity> activitiesIndex = new HashMap<>();
-
+  private Serializer serializer;
+      
   public PacemakerAPI()
   {
   }
+  
+  public PacemakerAPI(Serializer serializer)
+  {
+    this.serializer = serializer;
+  }
 
+  @SuppressWarnings("unchecked")
+  public void load() throws Exception
+  {
+    serializer.read();
+    activitiesIndex = (Map<Long, Activity>) serializer.pop();
+    emailIndex      = (Map<String, User>)   serializer.pop();
+    userIndex       = (Map<Long, User>)     serializer.pop();
+  }
+
+  void store() throws Exception
+  {
+    serializer.push(userIndex);
+    serializer.push(emailIndex);
+    serializer.push(activitiesIndex);
+    serializer.write(); 
+  }
   public Collection<User> getUsers ()
   {
     return userIndex.values();
   }
-
+  
   public  void deleteUsers() 
   {
     userIndex.clear();
     emailIndex.clear();
   }
-
+  
   public User createUser(String firstName, String lastName, String email, String password) 
   {
     User user = new User (firstName, lastName, email, password);
@@ -36,7 +69,7 @@ public class PacemakerAPI
     emailIndex.put(email, user);
     return user;
   }
-
+  
   public User getUserByEmail(String email) 
   {
     return emailIndex.get(email);
@@ -52,7 +85,7 @@ public class PacemakerAPI
     User user = userIndex.remove(id);
     emailIndex.remove(user.email);
   }
-
+  
   public Activity createActivity(Long id, String type, String location, double distance)
   {
     Activity activity = null;
@@ -65,12 +98,12 @@ public class PacemakerAPI
     }
     return activity;
   }
-
+  
   public Activity getActivity (Long id)
   {
     return activitiesIndex.get(id);
   }
-
+  
   public void addLocation (Long id, float latitude, float longitude)
   {
     Optional<Activity> activity = Optional.fromNullable(activitiesIndex.get(id));
@@ -78,5 +111,36 @@ public class PacemakerAPI
     {
       activity.get().route.add(new Location(latitude, longitude));
     }
+  }
+  
+  @SuppressWarnings("unchecked")
+  void load(File file) throws Exception
+  {
+    ObjectInputStream is = null;
+    try
+    {
+      XStream xstream = new XStream(new DomDriver());
+      is = xstream.createObjectInputStream(new FileReader(file));
+      userIndex       = (Map<Long, User>)     is.readObject();
+      emailIndex      = (Map<String, User>)   is.readObject();
+      activitiesIndex = (Map<Long, Activity>) is.readObject();
+    }
+    finally
+    {
+      if (is != null)
+      {
+        is.close();
+      }
+    }
+  }
+
+  void store(File file) throws Exception
+  {
+    XStream xstream = new XStream(new DomDriver());
+    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter(file));
+    out.writeObject(userIndex);
+    out.writeObject(emailIndex);
+    out.writeObject(activitiesIndex);
+    out.close(); 
   }
 }
